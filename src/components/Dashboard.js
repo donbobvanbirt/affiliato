acdc9edb55503a161b8d2e623210f2951e4dab3b
Donovan <donovan6925@gmail.com>
2016-11-14 18:48:07 -0800

    added post feed to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Form, Input, TextArea, Button, Container, Header } from 'semantic-ui-react';
+import { Form, Input, TextArea, Button, Container, Header, Feed } from 'semantic-ui-react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 
-import { submitPost } from '../actions/CampaignActions';
+import { submitPost, getCampaign } from '../actions/CampaignActions';
 
 const camp = '582a0dd9c28eb63519734218';
 
@@ -13,13 +14,11 @@ class Dashboard extends Component {
   }
 
   componentWillMount() {
-    
+    this.props.getCamp(camp);
   }
 
   submitForm = (e) => {
     e.preventDefault();
-    // const { title, body } = this.state;
-    // console.log('this.state:', this.state);
     this.props.addPost(this.state, camp);
   }
 
@@ -31,6 +30,36 @@ class Dashboard extends Component {
   }
 
   render() {
+    // const { posts } = this.props.campaign[0];
+    const campObj = this.props.campaign[0];
+    // console.log('this.props.campaign[0].posts:', this.props.campaign[0].posts);
+    // console.log('campObj.posts', campObj.posts);
+    let posts;
+    // let posts = campObj.posts || null;
+
+    let postFeed = 'You do not yet have any posts';
+
+    if (campObj) {
+      posts = campObj.posts.reverse();
+      postFeed = (
+        <Feed>
+          {posts.map((post, i) => {
+            const { title, body, timestamp } = post;
+            return (
+              <Feed.Event
+                key={i}
+                icon="pencil"
+                date={moment(timestamp).format('dddd MMM Do')}
+                summary={title}
+                extraText={body}
+              />
+            );
+          })}
+
+        </Feed>
+      );
+    }
+
     return (
       <Container>
         <Header as="h2">Add Post:</Header>
@@ -41,15 +70,23 @@ class Dashboard extends Component {
           <Form.Field control={TextArea} label="body" name="body" onChange={this.handleChange} placeholder="Start typing..." />
           <Form.Field control={Button}>Submit</Form.Field>
         </Form>
+
+        <Header as="h2">Your Posts:</Header>
+        {postFeed}
       </Container>
     );
   }
 }
 
+const mapStateToProps = state => ({ campaign: state.campaign });
+
 const mapDispatchToProps = dispatch => ({
   addPost(post, camp) {
     dispatch(submitPost(post, camp));
   },
+  getCamp(campaign) {
+    dispatch(getCampaign(campaign));
+  },
 });
 
-export default connect(null, mapDispatchToProps)(Dashboard);
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);

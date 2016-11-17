f0e382c8d827e074794eced45e7c3ba6aa8f8243
Donovan <donovan6925@gmail.com>
2016-11-16 17:37:20 -0800

    removed image from PostWidget

--- src/components/PostsWidget.js
+++ src/components/PostsWidget.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import { Card, Icon, Container, Image } from 'semantic-ui-react'
+import React from 'react';
+import { Card, Icon, Container, Image } from 'semantic-ui-react';
 
-const extra = (props) => (
+const extra = props => (
   <a>
-    <Icon name='comment outline' />
+    <Icon name="comment outline" />
     {props.timestamp}
   </a>
 );
 
-const PostsWidget = (props) => (
+const PostsWidget = props => (
   <Container>
-    {console.log('props:', props)}
 
-    <Image src={props.campaign.assets ? props.campaign.assets.profile : ''} shape="circular"/>
-    {props.campaign.posts.map((cur, i) => <Card key={i}
-      header={cur.title}
-      description={cur.body}
-      extra={extra(cur)}
-      fluid
-                                          />)}
+    {props.campaign.posts.map((cur, i) =>
+      <Card
+        key={i}
+        header={cur.title}
+        description={cur.body}
+        extra={extra(cur)}
+        fluid
+      />)}
 
   </Container>
-  )
+);
 
-  export default PostsWidget;
+export default PostsWidget;
